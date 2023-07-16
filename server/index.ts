1d2f1fb11b521238243f722f283f6ac6552da002
vika1968 <vika@teldan.com>
2023-07-16 17:50:30 +0300

    Gili

--- server/index.ts
+++ server/index.ts
@@ -11,15 +11,15 @@ const app = express();
 const port = process.env.PORT;
 app.use(cors(corsOptions))
 
-app.use((req, res, next) => {
-  // res.header('Access-Control-Allow-Origin', '*'); //The same
-  res.setHeader("Access-Control-Allow-Origin", "*"); //the same
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   // res.header('Access-Control-Allow-Origin', '*'); //The same
+//   res.setHeader("Access-Control-Allow-Origin", "*"); //the same
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
+//   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
 
 app.use(express.json());
